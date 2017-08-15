3f2df07f70e8a7fdafebd5241872925f726994e2
akshy07 <apate583@my.centennialcollege.ca>
2017-08-14 22:48:30 -0400

    Started Splashform first in driver class

--- BMI Calculator/SplashForm.Designer.cs
+++ BMI Calculator/SplashForm.Designer.cs
@@ -34,8 +34,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(302, 433);
+            this.ControlBox = false;
             this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
             this.Load += new System.EventHandler(this.SplashForm_Load);
             this.ResumeLayout(false);
