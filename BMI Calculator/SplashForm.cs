3b962df91974edb3f322e57683a312de12f25386
akshy07 <apate583@my.centennialcollege.ca>
2017-08-14 22:44:22 -0400

    Added Splash Form

---
+++ BMI Calculator/SplashForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Name: Akshay Patel
+ * 300918514
+ * Date: 14th August, 2017
+ * Description: This is a splash form.
+ * version 0.1 - Created a splashform.
+ */
+
+namespace BMI_Calculator
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
